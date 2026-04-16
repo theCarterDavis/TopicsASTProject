60e001b999cf9b6c1670ede2b613c1a86e3d3689
JB Pryor <jpryor35@icloud.com>
2026-04-16 13:24:34 -0700

    test to see github action working properly

--- hw2.py
+++ hw2.py
@@ -1,21 +1,20 @@
 def thisIsATest():
 	print("This Is A Test")
 
+def test():
+	print("test")
 
 def hello():
 	print("Hello World!")
 
 
-def yFunc():
+def yFunction():
 	y = 5
 	return y
 
 def ILikeMath():
 	x = 4+9
 	print(x)
-
-def anotherFunc():
-	print("another function added")
 
 hello()
 	
